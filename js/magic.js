6acae2a33968cdf5224d2f3853086559894b7d50
nathanliu1 <zero_2_6@hotmail.com>
2015-07-24 18:16:55 -0400

    First Commit

---
+++ js/magic.js
@@ -0,0 +1,25 @@
+$(function() {
+    var controller = new ScrollMagic.Controller();
+	 var aboutTween = TweenMax.staggerFromTo('#aboutme', 0.5,
+        {
+            y: 50,
+            x: -50,
+            opacity: 0
+        },
+        {
+            y: 0,
+            x: 0,
+            opacity: 1
+        },
+        0.2
+    );
+	var containerScene = new ScrollMagic.Scene({
+		triggerElement: '#part2',
+		offset: 100
+	})
+	.setTween(aboutTween)
+	.addIndicators()
+	.addTo(controller);
+	
+});
+
